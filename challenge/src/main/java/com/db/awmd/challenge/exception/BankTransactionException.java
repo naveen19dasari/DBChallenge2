75ffcc611526cd21f4341b8f0811031eaf864731
naveen19dasari <naveen.19dasari@gmail.com>
2020-06-23 10:21:59 +0530

    Commit

---
+++ challenge/src/main/java/com/db/awmd/challenge/exception/BankTransactionException.java
@@ -0,0 +1,8 @@
+package com.db.awmd.challenge.exception;
+
+public class BankTransactionException extends RuntimeException {
+	public BankTransactionException(String message) {
+	    super(message);
+	  }
+
+}
